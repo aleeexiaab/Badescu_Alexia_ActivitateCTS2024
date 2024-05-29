efab477cc5ded95de5e10c008ae7b140c530527e
aleeexia <alinda_floriana@yahoo.com>
2024-05-29 23:45:48 +0300

    .

--- D. Banca/src/Problem_2/Main.java
+++ D. Banca/src/Problem_2/Main.java
@@ -3,11 +3,11 @@ package Problem_2;
 public class Main {
     public static void main(String[] args) {
         ClientFactory individualFactory = new IndividualClientFactory();
-        Client individual = individualFactory.createClient("John Doe", "123456");
+        Client individual = individualFactory.createClient("Client 1", "123456");
         individual.getClientDetails();
 
         ClientFactory corporateFactory = new CorporateClientFactory();
-        Client corporate = corporateFactory.createClient("Acme Corp.", "987654321");
+        Client corporate = corporateFactory.createClient("Client 2", "987654321");
         corporate.getClientDetails();
     }
 }
